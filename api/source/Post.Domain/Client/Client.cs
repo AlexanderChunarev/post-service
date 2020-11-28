90149e3d812f464945502973eba46e868be2310b
VladGrushevoi <vlad.grush.99@gmail.com>
2020-11-29 00:02:03 +0200

    Add client model

---
+++ api/source/Post.Domain/Client/Client.cs
@@ -0,0 +1,11 @@
+namespace Post.Domain.Client
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }   
+    }    
+}
