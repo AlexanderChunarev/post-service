df9ad28d06ce3d885d8790346d219d203364b9b6
VladGrushevoi <vlad.grush.99@gmail.com>
2020-12-01 14:23:35 +0200

    Fix register client viewmodel

--- api/source/Post.WebApi/UseCases/Client/UpdateClientPresenter.cs
+++ api/source/Post.WebApi/UseCases/Client/UpdateClientPresenter.cs
@@ -3,7 +3,7 @@ using Post.Application.Boundaries.Client;
 
 namespace Post.WebApi.UseCases.Client
 {
-    public class UpdateClientPresenter : IOutputPort
+    public sealed class UpdateClientPresenter : IUpdateOutputPort
     {
         public IActionResult ViewModel { get; private set; }
         public void Error(string message)
