8c1e5b2ecf5ac0f3042789f9f448f17341e6a812
VladGrushevoi <vlad.grush.99@gmail.com>
2020-12-05 14:56:04 +0200

    Add methods

--- api/source/Post.Application/Repositories/Client/IClientRepository.cs
+++ api/source/Post.Application/Repositories/Client/IClientRepository.cs
@@ -1,10 +1,14 @@
 namespace Post.Application.Repositories.Client
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Post.Domain.Client;
+    using Post.Domain.Order;
     public interface IClientRepository 
     {
         Task Register(Client client);
         Task Update(int id, Client client);
+        Task<IEnumerable<Order>> GetDeparture(int idClient);
+        Task<IEnumerable<Order>> GetReceiving(string phoneNumber);
     }
 }
