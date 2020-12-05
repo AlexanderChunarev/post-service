dae41893dd3fec59ba315ca554f099a2af355f65
VladGrushevoi <vlad.grush.99@gmail.com>
2020-12-05 14:59:24 +0200

    Add presenter for departure/receiving orders

---
+++ api/source/Post.WebApi/UseCases/Client/ClientDepartutePresenter.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Post.Application.Boundaries.Order;
+
+namespace Post.WebApi.UseCases.Client
+{
+    public sealed class ClientDeparturePresenter : IOutputDepartureOrders
+    {
+        public IActionResult ViewModel { get; set; }
+        public void Error(string message)
+        {
+            ViewModel = new JsonResult(message);
+        }
+
+        public void Standard(CreateOrdersOutput output)
+        {
+            ViewModel = new JsonResult(output);
+        }
+    }
+}
