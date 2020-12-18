57cd1844aa2933e45f68635e4bad86fc6eedac18
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-12-18 21:13:07 +0200

    Update migrations

---
+++ api/source/Post.Infrastructure/DapperDataAccess/Migrations/Migration_202018122102.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Post.Infrastructure.DapperDataAccess.Migrations
+{
+    [Migration(202018122102)]
+    public class Migration_202018122102 : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("client").AddColumn("password").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("password").FromTable("client");
+        }
+    }
+}
