21a954d44481fbb821cf1be23cddcf4812db366a
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-12-18 21:10:40 +0200

    Update client input

--- api/source/Post.Application/Boundaries/Client/CreateClientInput.cs
+++ api/source/Post.Application/Boundaries/Client/CreateClientInput.cs
@@ -5,18 +5,20 @@ namespace Post.Application.Boundaries.Client
         public string Name { get; set; }
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }   
+        public string Email { get; set; }
+        public string Password { get; set; }
 
-        public CreateClientInput(string name, string surname, string phoneNumber, string email)
+        public CreateClientInput(string name, string surname, string phoneNumber, string email, string password)
         {
             Name = name;
             Surname = surname;
             PhoneNumber = phoneNumber;
             Email = email;
+            Password = password;
         }
 
-        public CreateClientInput(){
-
+        public CreateClientInput()
+        {
         }
     }
 }
