1b15259dd7874c6ad46742f61d49b9ceae4fcde8
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-12-18 21:09:47 +0200

    Create authentication use case

---
+++ api/source/Post.Application/UseCases/Authentication/AuthenticationUserUseCase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Post.Application.Boundaries.Authentication;
+using Post.Application.Repositories.Client;
+using Post.Application.Utils;
+
+namespace Post.Application.UseCases.Authentication
+{
+    using Post.Domain.Client;
+
+    public class AuthenticationUserUseCase : IAuthenticationUserUseCase
+    {
+        private readonly IClientRepository _clientRepository;
+        private readonly IOutputPort _outputHandler;
+
+        public AuthenticationUserUseCase(IClientRepository clientRepository, IOutputPort outputHandler)
+        {
+            _clientRepository = clientRepository;
+            _outputHandler = outputHandler;
+        }
+
+        public async Task Execute(AuthenticationInput input)
+        {
+            var client = await _clientRepository.GetClientByCredentials(
+                input.Login,
+                CryptUtils.EncryptPassword(input.Password)
+            );
+
+            if (client == null) return;
+            
+            _outputHandler.Standard(new AuthenticationOutput(
+                client.Id,
+                client.Name,
+                client.Surname,
+                client.Email,
+                ""));
+        }
+    }
+}
