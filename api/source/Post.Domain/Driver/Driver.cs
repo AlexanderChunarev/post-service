cb0a2109be6037f7f2382dae2c2405c5e9e8d4eb
VladGrushevoi <vlad.grush.99@gmail.com>
2020-12-18 22:46:12 +0200

    Add driver model

---
+++ api/source/Post.Domain/Driver/Driver.cs
@@ -0,0 +1,11 @@
+namespace Post.Domain.Driver
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public int CarId { get; set; }
+    }
+}
